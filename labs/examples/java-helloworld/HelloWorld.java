5095344315ac8370c802cddc9133154e165db246
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2020-02-24 14:31:36 +0530

    fixed helloworld

--- labs/examples/java-helloworld/HelloWorld.java
+++ labs/examples/java-helloworld/HelloWorld.java
@@ -1,47 +1,8 @@
 
-
 public class HelloWorld {
 	
-	Employee anil;
-	
 	public static void main(String[] args) {
-		
-		Integer a = new Integer(10);
-		int i = 10;
-		// byte, short, int, long, float, double, char, boolean
-		// String name = "capgemini"; // 9 bytes to 18 bytes
-		
-		System.out.println("Hello World!");
-	
-		//	Employee anil = null;	
-		
-		anil = new Employee(100, "Anil", 50000);
-		anilCopy = anil;
-		
-		anil = null;
-		
-		anil.printDetail()
-		
-	
-		
-		
+		System.out.println("Hello World!");		
 	}
 	
 }
-
-public abstract class Person {
-	final printDetails() {System.out.println(id + " " + name);}
-}
-
-public final class Employee extends Person{
-	static int id = 100; // 4
-	double salary; // 8
-	String name; // 8
-	
-	//public void printDetails() {
-//		
-	//}
-}
-
-
-
