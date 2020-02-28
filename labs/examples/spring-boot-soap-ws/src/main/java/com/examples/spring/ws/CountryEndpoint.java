640562c2a6a9f8002afcd788e31115ea728a76e6
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2020-02-28 19:14:22 +0530

    training materials

---
+++ labs/examples/spring-boot-soap-ws/src/main/java/com/examples/spring/ws/CountryEndpoint.java
@@ -0,0 +1,30 @@
+package com.examples.spring.ws;
+
+import com.examples.spring_boot_soap_ws.GetCountryRequest;
+import com.examples.spring_boot_soap_ws.GetCountryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = "http://examples.com/spring-boot-soap-ws";
+
+    private CountryRepository countryRepository;
+
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}
