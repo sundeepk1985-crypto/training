3ffb40845f20724b432009c24a8c214553f64df6
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2020-05-12 11:54:17 +0530

    ribbon load balancer changes

--- labs/examples/spring-cloud-ribbon/src/main/java/com/examples/spring/cloud/SpringCloudRibbonApplication.java
+++ labs/examples/spring-cloud-ribbon/src/main/java/com/examples/spring/cloud/SpringCloudRibbonApplication.java
@@ -7,10 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@RibbonClient(name="incident-service")
+@RestController
 public class SpringCloudRibbonApplication implements CommandLineRunner{
 	
 	@Autowired
@@ -29,9 +33,22 @@ public class SpringCloudRibbonApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
+		// STEP 1: Discover incident services running with service id incident-service from Eureka
+		// STEP 2: Load balance the request to different running instances
 		String response = restTemplate.getForObject("http://incident-service/incidents", String.class);
 		System.out.println(response);
 	}
+
+	@GetMapping("/tickets")
+	public String getTickets() {
+
+		// STEP 1: Discover incident services running with service id incident-service from Eureka
+		// STEP 2: Load balance the request to different running instances
+		String response = restTemplate.getForObject("http://incident-service/incidents", String.class);
+		System.out.println(response);
+		return response;
+	}
+
 
 }
